e24e22ca3465c48443cdc86f9b028d91037ed9c9
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-07-04 20:44:52 +0100

    add progress bar

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# scientific-research
+# Scientific-research
+
+## About
+
+Scientific research about the relationship between electromotive force (EMF) and the temperature in Daniel's cell solutions.
+We mainly focused on the comfort of the reader, also the styling of the website is inspired by [medium.com](https://medium.com/)
+
+## Stack used
+
+Html, css & JavaScript
+
+## Links
+
+- https://alialsyoufresearch.netlify.app/
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const faders = document.querySelectorAll('.fade-in');
+const nav = document.querySelector('.fake-nav');
+const hero = document.querySelector('.hero');
+const btn = document.querySelector('.btn');
+const filled = document.querySelector('.progress-bar');
 
 const appearOptions = {
   threshold: 0,
   rootMargin: '0px 0px -100px 0px',
+};
+
+const options = {
+  rootMargin: '-60px 0px 0px 0px',
 };
 
 const appearONScroll = new IntersectionObserver(function (
@@ -20,6 +28,29 @@ const appearONScroll = new IntersectionObserver(function (
 },
 appearOptions);
 
+const navScroll = new IntersectionObserver(function (entries, navScroll) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      nav.classList.remove('disappear');
+      btn.classList.remove('disappear');
+    } else {
+      nav.classList.add('disappear');
+      btn.classList.add('disappear');
+    }
+  });
+}, options);
+
+navScroll.observe(hero);
+
 faders.forEach((fader) => {
   appearONScroll.observe(fader);
 });
+
+function update() {
+  filled.style.width = `${
+    (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100
+  }%`;
+  requestAnimationFrame(update);
+}
+
+update();
